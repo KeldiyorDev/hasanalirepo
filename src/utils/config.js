c413bbad2c7f7b9c4a17bee5cab6652c73be74c5
99894 <keldiyorraxmatov1303@gmail.com>
2023-02-07 17:54:15 +0500

    push

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,9 +2,10 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
     baseURL: "http://localhost:8080/",
-    // headers: {
-    //     "Authorization": "Bearer " + localStorage.getItem("token")
-    // }
+    headers: {
+        // "Authorization": "Bearer " + localStorage.getItem("token");
+        'Content-Type': "application/x-www-form-urlencoded"
+    }
 });
 
 export const urlFile = "http://localhost:8080/";
